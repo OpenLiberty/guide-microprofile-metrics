331eea7fe4b41eeff0bfdd449225cf0741275cf2
Justine Chen <justinechen619@gmail.com>
2018-06-29 16:20:02 -0400

    Changed REST client to MP Rest Client  (#39)
    * Changed rest client/ Updated start dir / formatted
    
    * Removed defaultRestClient
    
    * Removed Imports/Config properties related to default rest client
    
    * Removed the defaultRestClient
    
    *  Removed extra method from InventoryManager and renamed remaining method for clarity
    
    * Removed config dependency from pom and server.xml

--- finish/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
+++ finish/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
@@ -14,7 +14,7 @@
 package io.openliberty.guides.inventory;
 
 import java.util.Properties;
-import io.openliberty.guides.inventory.client.SystemClient;
+
 import io.openliberty.guides.inventory.model.InventoryList;
 import javax.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.metrics.annotation.Counted;
@@ -28,17 +28,17 @@ import org.eclipse.microprofile.metrics.MetricUnits;
 public class InventoryManager {
 
   private InventoryList invList = new InventoryList();
-  private SystemClient systemClient = new SystemClient();
+  private InventoryUtils invUtils = new InventoryUtils();
 
   @Timed(name = "inventoryPropertiesRequestTime", absolute = true,
     description = "Time needed to get the properties of a system from the given hostname")
   public Properties get(String hostname) {
-    systemClient.init(hostname);
+    Properties properties = invUtils.getProperties(hostname);
 
-    Properties properties = systemClient.getProperties();
     if (properties != null) {
       invList.addToInventoryList(hostname, properties);
     }
+
     return properties;
 
   }
