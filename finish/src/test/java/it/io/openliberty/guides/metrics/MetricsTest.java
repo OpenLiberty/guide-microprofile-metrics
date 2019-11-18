fd1535a396ea28a9030fb96d2c3b21f446b9e749
DannyS03 <danny.salman@ryerson.ca>
2019-11-18 15:58:10 -0500

    hotspot & name convention update

--- finish/src/test/java/it/io/openliberty/guides/metrics/MetricsTest.java
+++ finish/src/test/java/it/io/openliberty/guides/metrics/MetricsTest.java
@@ -39,32 +39,51 @@ public class MetricsTest {
   private final String INVENTORY_HOSTNAME = "inventory/systems/localhost";
   private final String METRICS_APPLICATION = "metrics/application";
 
+  // tag::BeforeClass[]
   @BeforeClass
+  // end::BeforeClass[]
+  // tag::oneTimeSetup[]
   public static void oneTimeSetup() {
     httpPort = System.getProperty("liberty.test.port");
     httpsPort = System.getProperty("liberty.https.port");
     baseHttpUrl = "http://localhost:" + httpPort + "/";
     baseHttpsUrl = "https://localhost:" + httpsPort + "/";
   }
+  // end::oneTimeSetup[]
 
+  // tag::Before[]
   @Before
+  // end::Before[]
+  // tag::setup[]
   public void setup() {
     client = ClientBuilder.newClient();
+    // tag::JsrJsonpProvider[]
     client.register(JsrJsonpProvider.class);
+    // end::JsrJsonpProvider[]
   }
+  // end::setup[]
 
+  // tag::After[]
   @After
+  // end::After[]
+  // tag::teardown[]
   public void teardown() {
     client.close();
   }
+  // end::teardown[]
 
+  // tag::Test[]
   @Test
+  // end::Test[]
+  // tag::testSuite[]
   public void testSuite() {
     this.testPropertiesRequestTimeMetric();
     this.testInventoryAccessCountMetric();
     this.testInventorySizeGaugeMetric();
   }
+  // end::testSuite[]
 
+  // tag::testPropertiesRequestTimeMetric[]
   public void testPropertiesRequestTimeMetric() {
     connectToEndpoint(baseHttpUrl + INVENTORY_HOSTNAME);
     metrics = getMetrics();
@@ -76,7 +95,9 @@ public class MetricsTest {
       }
     }
   }
+  // end::testPropertiesRequestTimeMetric[]
 
+  // tag::testInventoryAccessCountMetric[]
   public void testInventoryAccessCountMetric() {
     connectToEndpoint(baseHttpUrl + INVENTORY_HOSTS);
     metrics = getMetrics();
@@ -87,7 +108,9 @@ public class MetricsTest {
       }
     }
   }
+  // end::testInventoryAccessCountMetric[]
 
+  // tag::testInventorySizeGaugeMetric[]
   public void testInventorySizeGaugeMetric() {
     metrics = getMetrics();
     for (String metric : metrics) {
@@ -98,6 +121,7 @@ public class MetricsTest {
     }
 
   }
+  // end::testInventorySizeGaugeMetric[]
 
   public void connectToEndpoint(String url) {
     Response response = this.getResponse(url);
