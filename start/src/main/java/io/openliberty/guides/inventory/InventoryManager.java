8b65b3835073de5ccb6c95a70e3dad23155330f2
Darren Zhang <darren.zhang@ibm.com>
2020-06-19 15:24:42 -0400

    Update inventory manager.

--- start/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
+++ start/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
@@ -1,22 +1,62 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2017, 2020 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
+// tag::InventoryManager[]
 package io.openliberty.guides.inventory;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
+
 import javax.enterprise.context.ApplicationScoped;
 
 import io.openliberty.guides.inventory.model.InventoryList;
+import io.openliberty.guides.inventory.model.SystemData;
 
+// tag::ApplicationScoped[]
 @ApplicationScoped
+// end::ApplicationScoped[]
 public class InventoryManager {
 
-	public Properties get(String hostname) {
-		return null;
-	}
+  private List<SystemData> systems = Collections.synchronizedList(new ArrayList<>());
+  private InventoryUtils invUtils = new InventoryUtils();
 
-	public void add(String hostname, Properties props) {	
-	}
+  // tag::get[]
+  public Properties get(String hostname) {
+    return invUtils.getProperties(hostname);
+  }
+  // end::get[]
 
-	public InventoryList list() {
-		return null;
-	}
-	
+  // tag::add[]
+  public void add(String hostname, Properties systemProps) {
+    Properties props = new Properties();
+    props.setProperty("os.name", systemProps.getProperty("os.name"));
+    props.setProperty("user.name", systemProps.getProperty("user.name"));
+
+    SystemData host = new SystemData(hostname, props);
+    if (!systems.contains(host))
+      systems.add(host);
+  }
+  // end::add[]
+
+  // tag::list[]
+  public InventoryList list() {
+    return new InventoryList(systems);
+  }
+  // end::list[]
+
+  // tag::getTotal[]
+  public int getTotal() {
+    return systems.size();
+  }
 }
