9b98119c3a2d8993203f0956b736f17f426db1f5
Gilbert Kwan <gkwan@ca.ibm.com>
2020-12-08 14:26:03 -0500

    Update MetricsIT.java

--- finish/src/test/java/it/io/openliberty/guides/metrics/MetricsIT.java
+++ finish/src/test/java/it/io/openliberty/guides/metrics/MetricsIT.java
@@ -24,6 +24,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.KeyStore;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
